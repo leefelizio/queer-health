201891a4ff043414bd32a61ff20db6a0ba6fad25
Lee Felizio <feliziolee@gmail.com>
2024-06-06 13:41:37 +0200

    routing update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,20 +1,18 @@
 import { Routes } from '@angular/router';
 import { dataResolver } from './resolvers/data.resolver';
 import { DetailComponent } from './containers/detail/detail.component';
-import { FavoritesComponent } from './containers/favorites/favorites.component';
 import { isAuthGuard } from './guards/is-auth.guard';
 import { NotFoundComponent } from './containers/not-found/not-found.component';
-import { LoginPageComponent } from './containers/login-page/login-page.component';
+import { LoginComponent } from './containers/login/login.component';
 import { isNotAuthGuard } from './guards/is-not-auth.guard';
 import { TabsComponent } from './containers/tabs/tabs.component';
-import { Tab1Component } from './containers/home/home.component';
 
 export const routes: Routes = [
-  {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
-  },
+    {
+      path: '',
+      redirectTo: 'home',
+      pathMatch: 'full'
+    },
     {
       path: '',
       component: TabsComponent,
@@ -30,10 +28,14 @@ export const routes: Routes = [
         {
           path: 'favorites',
           loadComponent: () => import('./containers/favorites/favorites.component').then(m => m.FavoritesComponent),
-          canLoad: [isAuthGuard],
-
+          canActivate: [isAuthGuard],
         },
       ],
+    },
+    {
+      path: 'login',
+      component: LoginComponent,
+
     },
     {
       path: 'professional',
@@ -44,12 +46,6 @@ export const routes: Routes = [
         }
       ],
       canActivate: [isAuthGuard],
-    },
-    {
-      path: 'login',
-      component: LoginPageComponent,
-      canActivate: [isNotAuthGuard],
-
     },
     {
       path: '404',
